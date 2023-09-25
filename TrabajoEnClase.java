f07ade1ee2233303c372cb1670e9443b5b154977
PauletteValle <143724538+PauletteValle@users.noreply.github.com>
2023-09-25 16:36:37 -0600

    Add files via upload

---
+++ TrabajoEnClase.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajoenclase;
+
+/**
+ *
+ * @author moram
+ */
+public class TrabajoEnClase {
+
+    public static void main(String[] args) {
+        
+        if (20>30){
+            System.out.println("La condicion se cumple");
+        }else{
+            System.out.println("La condicion no se cumple");
+        }
+    }
+}
